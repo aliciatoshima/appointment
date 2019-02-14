4ec9f8e116a3d23a5b2a77d36a437af89d5c325e
Alicia Toshima and Stephanie Wills <stephanie@>
2019-02-13 19:27:24 -0800

    did some more to calculator stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,59 @@
-//
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-// var result = add(number1, number2);
-// alert(result);
-//
+
+// Business (or back-end) logic:
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// User interface (or front-end) logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+   event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output1").text(result);
+  });
+
+  $("form#subtract").submit(function(event) {
+   event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $("#output2").text(result);
+  });
+
+  $("form#multiply").submit(function(event) {
+   event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output3").text(result);
+  });
+
+  $("form#divide").submit(function(event) {
+   event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+    $("#output4").text(result);
+  });
+});
+
+// subtract
 // var subtract = function(number1, number2) {
 //   return number1 - number2;
 // };
@@ -17,6 +63,8 @@
 // var result = subtract(number1, number2);
 // alert(result);
 //
+//
+// // multiply
 // var multiply = function(number1, number2) {
 //   return number1 * number2;
 // };
@@ -26,6 +74,8 @@
 // var result = multiply(number1, number2);
 // alert(result);
 //
+//
+// // divide
 // var divide = function(number1, number2) {
 //   return number1 / number2;
 // };
@@ -55,4 +105,4 @@
 // var result = ctof(celsius);
 // alert(result);
 
-var sample_regex = /epicodus/gi
+// var sample_regex = /epicodus/gi
